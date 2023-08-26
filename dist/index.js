1950527bffab163165d8fc543ec93f417015ef43
Your Name <you@example.com>
2023-08-26 21:00:38 +0530

    Functions and objects

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,13 @@ let level;
 let numbers = [1, 2, 3];
 numbers.forEach((n) => n * 2);
 let user = [1, "Dipankar"];
-user.push(1);
+var Size;
+
+(function (Size) {
+  Size[(Size["small"] = 1)] = "small";
+  Size[(Size["medium"] = 2)] = "medium";
+  Size[(Size["large"] = 3)] = "large";
+})(Size || (Size = {}));
+let mySize = Size.medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,34 @@ numbers.forEach((n) => n * 2);
 // 1, "Dipankar"
 //fixed string
 let user: [number, string] = [1, "Dipankar"];
-user.push(1);
+
+//ENUMS
+enum Size {
+  small = 1,
+  medium = 2,
+  large = 3,
+}
+let mySize: Size = Size.medium;
+console.log(mySize);
+
+//functions
+function calculateTax(income: number, taxYear?: number): number {
+  return (income += income + (taxYear || 303));
+}
+
+calculateTax(4000);
+
+//Objects
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "Dipankar",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
