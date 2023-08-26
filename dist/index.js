31ac2943a9ce0e4c57f65067feeb97e3b1011c17
Your Name <you@example.com>
2023-08-26 20:25:32 +0530

    arrays and tuples

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-let age = 20;
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+let numbers = [1, 2, 3];
+numbers.forEach((n) => n * 2);
+let user = [1, "Dipankar"];
+user.push(1);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-let age: number = 20;
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+//Using any but you should avoid it
+let level;
+
+//Arrays
+let numbers: number[] = [1, 2, 3];
+numbers.forEach((n) => n * 2);
+
+//tuples
+// 1, "Dipankar"
+//fixed string
+let user: [number, string] = [1, "Dipankar"];
+user.push(1);
